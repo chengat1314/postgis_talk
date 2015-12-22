986662e4d43e473bc7c999a87dffede31b5fe9a3
Parker Coleman <ADINSX.sa@gmail.com>
2015-12-21 22:46:39 -0800

    inital commit of lab contents

---
+++ sql_queries/center_city.sql
@@ -0,0 +1,14 @@
+﻿WITH fam_city AS(
+	SELECT name, geom FROM geo.cities_ne c WHERE c.name = 'New Orleans' 
+	OR c.name = 'Blacksburg' OR c.name = 'Seattle'
+)
+
+SELECT c.name || ', ' || c.adm1name AS loc, ST_DISTANCE(c.geom, 
+	(SELECT ST_CENTROID(
+		(SELECT ST_UNION(f.geom) FROM fam_city f))
+	), true) * 0.000621371 AS distance_miles
+	
+FROM geo.cities_ne c
+ORDER BY ST_DISTANCE(c.geom, (SELECT ST_CENTROID(
+		(SELECT ST_UNION(f.geom) FROM fam_city f)	
+	)), true)
---
+++ sql_queries/cities_cross_join.sql
@@ -0,0 +1,10 @@
+﻿SELECT 
+	c1.name || ', ' || c1.adm0name AS city1, 
+	c2.name || ', ' || c2.adm0name AS city2, 
+	ST_Distance(c1.geom, c2.geom, true) * 0.000621371 AS distance_miles
+	
+FROM geo.cities_ne c1
+CROSS JOIN geo.cities_ne c2
+WHERE c1.scalerank < 3 AND c2.scalerank < 3 --Limit the run time of the query by focusing on bigger cities
+ORDER BY ST_Distance(c1.geom, c2.geom, true) DESC
+LIMIT 10
